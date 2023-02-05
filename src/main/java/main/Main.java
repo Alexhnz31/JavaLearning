9a0af72cacd2da921313fba4ba528cf821d60ca7
Alexhnz31 <98045902+Alexhnz31@users.noreply.github.com>
2023-02-05 20:53:45 +0300

    Pass Person on Airplane

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,12 +1,12 @@
 package main;
 
-import lesson1.StringHandler;
+import lesson3.entity.Airport;
+
+import java.time.LocalDateTime;
 
 public class Main {
-    public static void main(String[] args) throws IllegalAccessException {
-        String string = "";
-        String string1 = " негр";
-        StringHandler stringHandler = new StringHandler();
-        System.out.println(stringHandler.countSymbols(""));
+    public static void main(String[] args){
+        Airport airport1 = new Airport();
+        airport1.passPersonToAirplane(LocalDateTime.of(2023, 3, 8, 16, 0), LocalDateTime.of(2023, 3, 8, 16, 0), LocalDateTime.of(2023, 3, 8, 17, 0));
     }
 }
